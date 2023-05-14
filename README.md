f4c5d16e530d34f2e3fe8f6e6277854cf21f8660
10cyrilc <10cyrilc@gmail.com>
2023-05-14 12:33:31 +0530

    [Github] First Github Commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Full Screen Clock
+
+A simple full-screen clock app built with React Native and Expo. Stay aware of the time with this minimalist and elegant clock display.
+
+## Features
+
+- Full-screen clock display with large digits
+- Digital clock format with hours, minutes, and seconds
+- Automatic time updates
+- Customizable clock color and background
+
+## Screenshot
+
+![Screenshot](https://github.com/10cyrilc/full-screen-clock/screenshots/screen.png)
+
+## Getting Started
+
+1. Clone the repository to your local machine.
+
+```
+git clone https://github.com/10cyrilc/full-screen-clock.git
+```
+
+2. Navigate to the project directory.
+
+```
+cd full-screen-clock
+```
+
+3. Install the required dependencies.
+
+```
+npm install
+```
+
+4. Start the Expo development server.
+
+```
+npm start
+```
+
+5. Scan the QR code using the Expo Go app on your mobile device or run it in a simulator.
+
+## Built With
+
+- [React Native](https://reactnative.dev/) - A framework for building native apps using React.
+- [Expo](https://expo.io/) - A platform for building and deploying universal, native-like apps.
+
+## Contributing
+
+If you are interested in contributing to this project, feel free to fork the repository and submit a pull request. Your contributions are always welcome!
+
+## License
+
+This project is licensed under the MIT License.
+
+## Authors
+
+- [@10cyrilc](https://www.github.com/10cyrilc)
+
+## Support Me
+
+<a href="https://www.buymeacoffee.com/10cyrilc" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
---
+++ js/Timer.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { RFPercentage } from "react-native-responsive-fontsize";
+import { useKeepAwake } from "expo-keep-awake";
+
+const Timer = () => {
+  useKeepAwake();
+
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentDay, setCurrentDay] = useState("");
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const [fontsLoaded] = useFonts({
+    MissionDanger: require("../assets/font/missiondanger.ttf"),
+    OriginTech: require("../assets/font/OriginTech.otf"),
+    Mechsuit: require("../assets/font/Mechsuit.otf"),
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const date = new Date();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+      const calendarDate = date.getDate();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const day = weekday[date.getDay()];
+
+      const formattedTime = `${hours < 10 ? "0" : ""}${hours}:${
+        minutes < 10 ? "0" : ""
+      }${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+
+      const formattedDay = `${calendarDate}-${month}-${year}`;
+
+      setCurrentTime(formattedTime);
+      setCurrentDate(formattedDay);
+      setCurrentDay(day);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  if (fontsLoaded) {
+    return (
+      <>
+        <StatusBar hidden={true} />
+        <View style={styles.container}>
+          <Text style={styles.clock}>{currentTime}</Text>
+          <View>
+            <Text
+              style={[
+                styles.clock,
+                {
+                  fontFamily: "Mechsuit",
+                  fontSize: RFPercentage(3),
+                },
+              ]}
+            >
+              {currentDate}
+            </Text>
+            <Text
+              style={[
+                styles.clock,
+                {
+                  fontFamily: "MissionDanger",
+                  fontSize: RFPercentage(5),
+                },
+              ]}
+            >
+              {currentDay}
+            </Text>
+          </View>
+        </View>
+      </>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text> Loading......</Text>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  clock: {
+    fontSize: RFPercentage(20),
+    fontFamily: "OriginTech",
+    textAlign: "center",
+    color: "#00FF00",
+  },
+});
+
+export default Timer;
